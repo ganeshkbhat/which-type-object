2c00fa652ae2f88e087990d0ad99dbaf3749a18c
Ganesh B <ganeshsurfs@gmail.com>
2024-03-05 09:22:39 +0530

    [COMMIT] Adding packages and first commit for the package

--- test/test.types.js
+++ test/test.types.js
@@ -17,28 +17,12 @@
 
 
 const { expect } = require('chai');
-const { tagTester,
-  isBoolean,
-  isObject,
-  isNull,
-  isUndefined,
+const { TagTester,
   isElement,
-  isString,
-  isNumber,
-  isDate,
-  isRegExp,
-  isError,
-  isSymbol,
-  isArrayBuffer,
-  isFunction,
-  isDataView,
-  hasDataViewBug,
-  isValidDataView,
-  hasObjectTag,
   isBufferLike } = require('../index');
 
 
-describe('[is typed array tests] index.js Tests to check the type of an object using tagTester', () => {
+describe('[is typed array tests] index.js Tests to check the type of an object using TagTester', () => {
   let tst, written;
   before(() => {
 
@@ -55,104 +39,104 @@ describe('[is typed array tests] index.js Tests to check the type of an object u
   // isTypedArrayUsingPattern,
   // toBufferView,
   // getShallowProperty,
-  // tagTester
+  // TagTester
 
-  it('should get String type checker using tagTester', () => {
+  it('should get String type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isString = tagTester('String');
+    var isString = TagTester('String');
     expect(isString("tester")).to.equal(expected);
   });
 
-  it('should get Number type checker using tagTester', () => {
+  it('should get Number type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isNumber = tagTester('Number');
+    var isNumber = TagTester('Number');
     expect(isNumber(1)).to.equal(expected);
   });
 
-  it('should get Boolean type checker using tagTester', () => {
+  it('should get Boolean type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isBoolean = tagTester('Boolean');
+    var isBoolean = TagTester('Boolean');
     expect(isBoolean(true)).to.equal(expected);
   });
 
-  it('should get Date type checker using tagTester', () => {
+  it('should get Date type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isDate = tagTester('Date');
+    var isDate = TagTester('Date');
     expect(isDate(Date.now())).to.equal(!expected);
     expect(isDate(new Date(Date.now()))).to.equal(expected);
   });
 
-  it('should get Array type checker using tagTester', () => {
+  it('should get Array type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isArray = tagTester('Array');
+    var isArray = TagTester('Array');
     expect(isArray([1,2,3])).to.equal(expected);
   });
-  it('should get Array type checker using tagTester', () => {
+  it('should get Array type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isRegExp = tagTester('RegExp');
+    var isRegExp = TagTester('RegExp');
     expect(isRegExp(new RegExp(/^d/g))).to.equal(expected);
     expect(isRegExp(/^d/)).to.equal(expected);
   });
 
-  it('should get Error type checker using tagTester', () => {
+  it('should get Error type checker using TagTester', () => {
     let actual = true;
     let expected = true;
-    var isError = tagTester('Error');
+    var isError = TagTester('Error');
     expect(isError(Error)).to.equal(!expected); // false not instantiated class
     expect(isError(new Error("Tester"))).to.equal(expected);
   });
 
-  it('should get Symbol type checker using tagTester', () => {
+  it('should get Symbol type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isSymbol = tagTester('Symbol');
+    var isSymbol = TagTester('Symbol');
     expect(isSymbol(Symbol("Tester"))).to.equal(expected);
   });
 
-  it('should get ArrayBuffer type checker using tagTester', () => {
+  it('should get ArrayBuffer type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isArrayBuffer = tagTester('ArrayBuffer');
+    var isArrayBuffer = TagTester('ArrayBuffer');
     expect(isArrayBuffer([])).to.not.equal(expected); // false // is an array not ArrayBuffer
     expect(isArrayBuffer(Buffer.from([]))).to.not.equal(expected);
     expect(isArrayBuffer(new ArrayBuffer([]))).to.equal(expected);
   });
 
-  it('should get Function type checker using tagTester', () => {
+  it('should get Function type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isFunction = tagTester('Function');
+    var isFunction = TagTester('Function');
     expect(isFunction(()=>{})).to.equal(expected);
   });
 
-  it('should get DataView type checker using tagTester', () => {
+  it('should get DataView type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var isDataView = tagTester('DataView');
+    var isDataView = TagTester('DataView');
     expect(isDataView([])).to.equal(!expected);
   });
 
-  it('should get Object type checker using tagTester', () => {
+  it('should get Object type checker using TagTester', () => {
     let actual = true;
     let expected = true;
 
-    var hasObjectTag = tagTester('Object');
+    var hasObjectTag = TagTester('Object');
     expect(hasObjectTag({"sc": 10 })).to.equal(expected);
   });
 
